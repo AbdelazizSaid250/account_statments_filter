9d4bcbef8ab96e2362c99c4ce57a1da34789e970
abdelaziz <abdelazizsaid250@gmail.com>
2022-09-12 17:31:03 +0400

    - Unit Test

--- account_statement_service/src/test/java/com/master/engineeringtool/EngineeringToolApplicationTests.java
+++ account_statement_service/src/test/java/com/master/engineeringtool/EngineeringToolApplicationTests.java
@@ -3,12 +3,22 @@ package com.master.engineeringtool;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.master.common.util.Const.YMD_FORMAT;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class EngineeringToolApplicationTests {
 
     @Test
     void contextLoads() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(YMD_FORMAT);
+        int monthValue = LocalDate.parse("1994-09-19", formatter).getMonthValue();
+
+        assertEquals(9, monthValue);
     }
 
 }
